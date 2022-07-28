5a28a98fb1d08256ae78375a75be7f3486c3528c
lesson <1565510080@qq.com>
2022-07-28 23:05:16 +0800

    feat: return type

--- medium/Get-Return-Type/template.ts
+++ medium/Get-Return-Type/template.ts
@@ -1,5 +1,5 @@
-type MyReturnType<T extends (...args:any[])=>any> =
- T extends (...arg:any[])=> infer R ? R: never
+// type MyReturnType<T extends (...args:any[])=>any> =
+//  T extends (...arg:any[])=> infer R ? R: never
 
 
 
@@ -7,3 +7,12 @@ type MyReturnType<T extends (...args:any[])=>any> =
 //  如果 infer 推断出来 则直接采用 推断不出来 则就是 never (什么也不做)
 
 
+type MyReturnType<T extends (...arg:any[])=>any> = T extends (...arg:any[])=>infer F?F:never
+
+/* 
+  处理一个函数的返回值类型 
+
+  首先是一个函数  测试用例上 会对函数 添加参数 
+  infer 推断
+
+*/
